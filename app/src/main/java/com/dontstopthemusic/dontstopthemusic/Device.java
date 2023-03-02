21b867c92b5306cdeb334925c468cf5b36fbb75d
ldh35 <ldh35@cam.ac.uk>
2023-03-02 12:05:58 +0000

    Stopped device from emitting null data

--- app/src/main/java/com/dontstopthemusic/dontstopthemusic/Device.java
+++ app/src/main/java/com/dontstopthemusic/dontstopthemusic/Device.java
@@ -131,7 +131,8 @@ public class Device implements AutoCloseable
 		synchronized ( mStatusChangeCallbacks )
 		{
 			mStatusChangeCallbacks.add ( callback );
-			callback.onStatusChange ( this );
+			if ( mMostRecentData != null )
+				callback.onStatusChange ( this );
 		}
 	}
 
